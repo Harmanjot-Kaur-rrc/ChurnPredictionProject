b199a34537bf9360521475b520ca2c28e9747a11
Harmanjot-Kaur-rrc <harmanjot082003@gmail.com>
2026-02-17 13:37:01 -0600

    Fix merge conflict in voting.py

--- src/models/voting.py
+++ src/models/voting.py
@@ -1,21 +1,18 @@
 from sklearn.ensemble import VotingClassifier
-<<<<<<< HEAD
 
-=======
- 
- 
->>>>>>> c3ef9a4760b0bb7f096cf15f5ff9acc26640b276
+
 def build_voting(trained_models):
- 
+
     estimators = [
         ("log", trained_models["Logistic"].best_estimator_),
         ("rf", trained_models["RandomForest"].best_estimator_),
         ("gb", trained_models["GradientBoosting"].best_estimator_),
-        ("xgb", trained_models["XGBoost"].best_estimator_)
+        ("xgb", trained_models["XGBoost"].best_estimator_),
     ]
- 
+
     return VotingClassifier(
         estimators=estimators,
         voting="soft",
-        weights=[1, 1, 1, 2]
+        weights=[1, 1, 1, 2],
     )
+
